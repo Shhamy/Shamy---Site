efffbdcc3b52a8361365a41f4970a9d090e9d92e
Shhamy <104292834+Shhamy@users.noreply.github.com>
2026-06-01 05:39:09 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const bookingForm = document.querySelector("#booking-form");
-const formNext = document.querySelector("#form-next");
-
-if (bookingForm && formNext) {
-  bookingForm.addEventListener("submit", () => {
-    const currentPath = window.location.pathname;
-    const siteFolder = currentPath.slice(0, currentPath.lastIndexOf("/") + 1);
-    formNext.value = `${window.location.origin}${siteFolder}merci.html`;
-  });
-}
+// Reserved for future site interactions.
